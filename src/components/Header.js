3e702e9ebc1b89243073cbc57b7dd8f636c15446
vivekmehto <vivekmehto.webdev@gmail.com>
2023-09-01 23:10:02 +0530

    FIX: Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import logo from "../assets/logo.png";
 const Header = () => {
   return (
-    <div className="header">
-      <img className="logo" src={logo} alt="react-food-logo" />
+    <div className="flex justify-between">
+      <img className="w-24 h-24 " src={logo} alt="react-food-logo" />
 
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+      <ul className="flex">
+        <li className="m-6">Home</li>
+        <li className="m-6">About</li>
+        <li className="m-6">Contact</li>
       </ul>
 
-      <button>Login</button>
+      <button className=" bg-blue-400 hover:bg-blue-700 mx-1 my-auto p-2 rounded-sm">
+        Login
+      </button>
     </div>
   );
 };

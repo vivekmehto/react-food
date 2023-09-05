abcd900ff30840d773feb2fedca6bb5d00b76dbd
vivekmehto <vivekmehto.webdev@gmail.com>
2023-09-05 15:11:10 +0530

    Menu page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,125 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  IMG_URL,
+  MENU_API,
+  MENU_ITEM_TYPE_KEY,
+  RESTAURANT_TYPE_KEY,
+} from "../constants";
+import { MenuShimmer } from "./Shimmer";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  const { resId } = useParams();
+  const [restaurant, setRestaurant] = useState(null);
+  const [restaurantMenu, setRestaurantMenu] = useState([]);
+
+  useEffect(() => {
+    getRestaurantMenu();
+  }, []);
+
+  async function getRestaurantMenu() {
+    try {
+      const response = await fetch(MENU_API + resId);
+      const json = await response.json();
+
+      const restaurantData =
+        json?.data?.cards
+          ?.map((x) => x.card)
+          ?.find((x) => x && x.card["@type"] === RESTAURANT_TYPE_KEY)?.card
+          ?.info || null;
+      setRestaurant(restaurantData);
+
+      console.log(restaurantData);
+      const menuItemsData =
+        json?.data?.cards
+          .find((x) => x.groupedCard)
+          ?.groupedCard?.cardGroupMap?.REGULAR?.cards?.map((x) => x.card?.card)
+          ?.filter((x) => x["@type"] == MENU_ITEM_TYPE_KEY)
+          ?.map((x) => x.itemCards)
+          .flat()
+          .map((x) => x.card?.info) || [];
+
+      const uniqueMenuItems = [];
+      menuItemsData.forEach((item) => {
+        if (!uniqueMenuItems.find((x) => x.id === item.id)) {
+          uniqueMenuItems.push(item);
+        }
+      });
+      console.log(uniqueMenuItems);
+      setRestaurantMenu(uniqueMenuItems);
+    } catch (error) {
+      setRestaurantMenu([]);
+      setRestaurant(null);
+      console.log(error);
+    }
+  }
+
+  const getBgColor = () => {
+    if (restaurant.avgRating >= 3.5) {
+      return "green";
+    } else {
+      return "red";
+    }
+  };
+  return restaurant === null ? (
+    <MenuShimmer />
+  ) : (
+    <div className="">
+      <div className="bg-gray-700 flex justify-center items-center">
+        <img
+          src={IMG_URL + restaurant?.cloudinaryImageId}
+          alt={"REACT FOOD " + restaurant?.name}
+          className="w-72 h-80 rounded-2xl m-4 p-4"
+        />
+        <div className="leading-8 text-white">
+          <h1 className="font-semibold text-4xl">{restaurant?.name}</h1>
+          <div
+            style={{ background: getBgColor() }}
+            className="border-gray-500 border-solid border-1 my-2 w-max rounded-md px-1 text-white "
+          >
+            ☆ {restaurant?.avgRating}
+          </div>
+          {restaurant?.cuisines.map((i) => (
+            <span className="font-light" key={i}>
+              {" "}
+              {i}
+            </span>
+          ))}
+
+          <p className="text-lg">
+            {restaurant?.availabilityServiceabilityMessage}
+          </p>
+          <p className="text-lg">{restaurant?.costForTwoMessage}</p>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        {restaurantMenu.map((item) => (
+          <div
+            key={item.id}
+            className="flex justify-between items-center w-[60%] m-3 p-3"
+          >
+            <div className="w-[60%]">
+              <p>{item?.isVeg ? " 🟢 Veg" : " 🔴 Non-Veg"}</p>
+              <p className="font-semibold text-xl">{item?.name}</p>
+
+              <p className="font-light ">{item?.description}</p>
+              <p className="font-medium text-lg">{item?.price}</p>
+            </div>
+            <div className="flex flex-col justify-between items-center ">
+              <img
+                src={IMG_URL + (item?.imageId || null)}
+                alt={"REACT FOOD " + item?.name}
+                className="w-40 h-40 rounded-lg"
+              />
+              <button className="border-white bg-red-400 border-solid border-2 rounded-md p-2 m-2">
+                ADD
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Menu;

b71c0606927127ea94774ad1b8bf77d12599ed4f
vivekmehto <vivekmehto.webdev@gmail.com>
2023-09-08 22:18:30 +0530

    Refactor: Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import logo from "../assets/logo.png";
+import logo2 from "../assets/logo2-bg.png";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="flex justify-between">
-      <img className="w-24 h-24 " src={logo} alt="react-food-logo" />
-
+    <div className="flex justify-between w-[90%] m-auto">
+      <div className="flex justify-center items-center">
+        <img className="h-16" src={logo2} alt="react-food-logo" />
+        <h1 className="text-2xl font-semibold uppercase">React Food</h1>
+      </div>
       <ul className="flex">
         <li className="m-6">
           <Link to="/">Home</Link>
@@ -17,9 +19,9 @@ const Header = () => {
         </li>
       </ul>
 
-      <button className=" bg-blue-400 hover:bg-blue-700 mx-1 my-auto p-2 rounded-sm">
+      {/* <button className=" bg-blue-400 hover:bg-blue-700 mx-1 my-auto p-2 rounded-sm">
         Login
-      </button>
+      </button> */}
     </div>
   );
 };
